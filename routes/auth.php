1d12ca9942074514d5bd63b647b19b2b0fe999d5
abrajelakotobia <103642520+abrajelakotobia@users.noreply.github.com>
2025-05-17 19:01:55 +0100

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
